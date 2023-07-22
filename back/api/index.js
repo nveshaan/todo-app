52156f02a0e07f8fc7aa8700720406f1e42ccaca
nveshaan <nveshaan@gmail.com>
2023-07-22 14:22:02 +0530

    vercel deploy

--- back/api/index.js
+++ back/api/index.js
@@ -3,26 +3,24 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 
+const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../public")));
 
 let todos = [];
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.get("/todos", (req, res) => {
   res.json(todos);
 });
-
-// app.get("/todos/:id", (req, res) => {
-//   const todo = todos.find((t) => t.id === parseInt(req.params.id));
-//   if (!todo) {
-//     res.status(404).send();
-//   } else {
-//     res.json(todo);
-//   }
-// });
 
 app.post("/todos", (req, res) => {
   const newTodo = {
@@ -32,16 +30,6 @@ app.post("/todos", (req, res) => {
   todos.push(newTodo);
   res.json(todos);
 });
-
-// app.put("/todos/:id", (req, res) => {
-//   const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
-//   if (todoIndex === -1) {
-//     res.status(404).send();
-//   } else {
-//     todos[todoIndex].description = req.body.description;
-//     res.json(todos[todoIndex]);
-//   }
-// });
 
 app.post("/todos/:id", (req, res) => {
   const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
@@ -58,6 +46,28 @@ app.use((req, res, next) => {
   res.status(404).send();
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+module.exports = app;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}
+  -> http://localhost:${port}`);
 });
+
+// app.get("/todos/:id", (req, res) => {
+//   const todo = todos.find((t) => t.id === parseInt(req.params.id));
+//   if (!todo) {
+//     res.status(404).send();
+//   } else {
+//     res.json(todo);
+//   }
+// });
+
+// app.put("/todos/:id", (req, res) => {
+//   const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+//   if (todoIndex === -1) {
+//     res.status(404).send();
+//   } else {
+//     todos[todoIndex].description = req.body.description;
+//     res.json(todos[todoIndex]);
+//   }
+// });
